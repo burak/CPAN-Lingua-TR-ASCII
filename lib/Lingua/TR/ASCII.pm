8ee9705a7c741d46d8842f6766a3239d1d13ecb5
Burak Gürsoy <burak@cpan.org>
2011-01-21 01:06:39 +0100

    Clean up

--- lib/Lingua/TR/ASCII.pm
+++ lib/Lingua/TR/ASCII.pm
@@ -18,10 +18,11 @@ sub turkish_to_ascii {
 }
 
 sub _new {
-    my($class, $ascii_string) = @_;
+    my($class, $input) = @_;
     my $self = {
-        ascii_string   => $ascii_string,
-        turkish_string => $ascii_string,
+        input   => $input,
+        length  => length $input,
+        turkish => $input,
     };
     bless $self, $class;
     return $self;
@@ -31,38 +32,34 @@ sub _deasciify {
     my($self) = @_;
     # Convert a string with ASCII-only letters into one with
     # Turkish letters.
-    my @chars = split m{}xms, $self->{turkish_string};
+    my $s     = \$self->{turkish};
+    my @chars = split m{}xms, ${$s};
 
     for my $i ( 0 .. $#chars ) {
         my $c = $chars[$i];
-        if ( $self->_needs_correction( $c, $i ) ) {
-            substr $self->{turkish_string}, $i, 1, $TOGGLE_ACCENT->{ $c } || $c;
-        }
+        next if ! $self->_needs_correction( $c, $i );
+        substr ${$s}, $i, 1, $TOGGLE_ACCENT->{ $c } || $c;
     }
 
-    return $self->{turkish_string};
+    return ${$s};
 }
 
 sub _needs_correction {
     my($self, $ch, $point) = @_;
-    $point ||= 0;
     # Determine if char at cursor needs correction.
     my $tr = $ASCIIFY->{ $ch } || $ch;
     my $pl = $PATTERN->{ lc $tr };
-    my $m  = $pl ? $self->_matches($pl, $point) : 0;
+    my $m  = $pl ? $self->_matches( $pl, $point || 0 ) : 0;
 
-    return $tr eq 'I'
-                ? ( $ch eq $tr ? ! $m :   $m )
-                : ( $ch eq $tr ?   $m : ! $m )
-                ;
+    return $tr eq 'I' ? ( $ch eq $tr ? ! $m :   $m )
+                      : ( $ch eq $tr ?   $m : ! $m );
 }
 
 sub _matches {
     my($self, $dlist, $point) = @_;
-    $point ||= 0;
     # Check if the pattern is in the pattern table.
     my $rank  = 2 * keys %{ $dlist };
-    my $str   = $self->_get_context( $point );
+    my $str   = $self->_get_context( $point || 0 );
     my $start = 0;
     my $end   = 0;
 
@@ -88,53 +85,47 @@ sub _get_context {
     my $s = q{ } x ( 1 + ( 2 * $size ) );
     substr $s, $size, 1, 'X';
 
+    my $turkish = \$self->{turkish};
     my $i       = 1 + $size;
-    my $index   = $point;
+    my $index   = 1 + $point;
+    my $len     = $self->{length};
     my $space;
 
-    my $current_char = substr $self->{turkish_string}, $index, 1;
-
-    $index++;
-
-    while ( $i < length($s) && ! $space && $index < length($self->{ascii_string}) ) {
-        $current_char = substr $self->{turkish_string}, $index, 1;
+    while ( $i < length $s && ! $space && $index < $len ) {
+        my $current_char = substr $self->{turkish}, $index++, 1;
         my $x = $DOWNCASE_ASCIIFY->{ $current_char };
         if ( ! $x ) {
             if ( ! $space ) {
-                $i++;
                 $space = 1;
+                $i++;
             }
         }
         else {
             substr $s, $i, 1, $x;
-            $i++;
             $space = 0;
+            $i++;
         }
-        $index++;
     }
 
     $s     = substr $s, 0, $i;
-    $index = $point;
-    $i     = $size - 1;
+    $index = $point - 1;
+    $i     = $size  - 1;
     $space = 0;
-    $index--;
 
     while ( $i >= 0 && $index >= 0 ) {
-        $current_char = substr $self->{turkish_string}, $index, 1;
+        my $current_char = substr $self->{turkish}, $index--, 1;
         my $x = $UPCASE_ACCENTS->{ $current_char };
-
         if ( ! $x ) {
             if ( ! $space ) {
-                $i--;
                 $space = 1;
+                $i--;
             }
         }
         else {
             substr $s, $i, 1, $x;
-            $i--;
             $space = 0;
+            $i--;
         }
-        $index--;
     }
 
     return $s;
